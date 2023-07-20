2756f134849b4a83971abef06ed11b8c60d1bdb2
jeffrey.douglas <jeffrey.douglas@sbevision.com>
2023-07-20 17:10:56 -0400

    Add logo and title

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import { useState } from 'react'
 import About from './about/About'
 import Experience from './experience/Experience'
 import Projects from './projects/Projects'
+import Head from 'next/head'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -26,27 +27,33 @@ export default function Home() {
   const [sectionIndex, setSectionIndex] = useState(0)
 
   return (
-    <main className="flex min-h-screen flex-col items-center p-4 lg:p-10">
-      <div className='flex flex-col items-center lg:flex-row lg:justify-between lg:min-w-full'>
-        <div className='flex flex-col items-center lg:items-start'>
-          <h1 className={`${inter.className} text-4xl font-semibold`}>Hello</h1>
-          <p className='text-lg pb-2 lg:pb-0'>Lars Here</p>
+    <div>
+      <Head>
+        <title>Larry Fisher Labs</title>
+        <meta name='description' content='The personal website of Lars.'/>
+      </Head>
+      <main className="flex min-h-screen flex-col items-center p-4 lg:p-10">
+        <div className='flex flex-col items-center lg:flex-row lg:justify-between lg:min-w-full'>
+          <div className='flex flex-col items-center lg:items-start'>
+            <h1 className={`${inter.className} text-4xl font-semibold`}>Hello</h1>
+            <p className='text-lg pb-2 lg:pb-0'>Lars Here</p>
+          </div>
+          <div className='rounded-xl inline-flex h-8'>
+            <button className={getNavButtonClass(0, sectionIndex)} onClick={() => setSectionIndex(0)}>About</button>
+            <button className={getNavButtonClass(1, sectionIndex)} onClick={() => setSectionIndex(1)}>Experience</button>
+            <button className={getNavButtonClass(2, sectionIndex)} onClick={() => setSectionIndex(2)}>Projects</button>
+          </div>
         </div>
-        <div className='rounded-xl inline-flex h-8'>
-          <button className={getNavButtonClass(0, sectionIndex)} onClick={() => setSectionIndex(0)}>About</button>
-          <button className={getNavButtonClass(1, sectionIndex)} onClick={() => setSectionIndex(1)}>Experience</button>
-          <button className={getNavButtonClass(2, sectionIndex)} onClick={() => setSectionIndex(2)}>Projects</button>
-        </div>
-      </div>
-      {
-        sectionIndex === 0 ? (
-          <About />
-        ) : sectionIndex === 1 ? (
-          <Experience />
-        ) : (
-          <Projects />
-        )
-      }
-    </main>
+        {
+          sectionIndex === 0 ? (
+            <About />
+          ) : sectionIndex === 1 ? (
+            <Experience />
+          ) : (
+            <Projects />
+          )
+        }
+      </main>
+    </div>
   )
 }
