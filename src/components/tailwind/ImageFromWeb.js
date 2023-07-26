0d6cf57f05df00631897da87021404529503474a
jeffrey.douglas <jeffrey.douglas@sbevision.com>
2023-07-26 14:55:00 -0400

    Add useEffect for creation of image objects.

--- src/components/tailwind/ImageFromWeb.js
+++ src/components/tailwind/ImageFromWeb.js
@@ -1,21 +1,22 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 export default function ImageFromWeb({ imgUrl, imgAlt }) {
     const [isLoaded, setIsLoaded] = useState(false)
-    const imgFromWeb = new Image()
-    imgFromWeb.onload = () => {
-        imgFromWeb.className = 'rounded-lg w-[3rem] h-[3rem] lg:w-[6.4rem] lg:h-[6.4rem]'
-        imgFromWeb.alt = imgAlt
-        setIsLoaded(true)
-    }
-    imgFromWeb.src = imgUrl
+
+    useEffect(() => {
+        const imgFromWeb = new Image()
+        imgFromWeb.onload = () => {
+            setIsLoaded(true)
+        }
+        imgFromWeb.src = imgUrl
+    })
 
     return (
         <div>
             {isLoaded === true ? (
                 <img className='rounded-lg w-[3rem] h-[3rem] lg:w-[6.4rem] lg:h-[6.4rem]' src={imgUrl} alt={imgAlt}/>
             ) : (
-                <div className='w-[3rem] h-[3rem] lg:w-[6.4rem] lg:h-[6.4rem]'>
+                <div className='flex items-center justify-center w-[3rem] h-[3rem] border rounded-lg lg:w-[6.4rem] lg:h-[6.4rem]'>
                     <p className='text-3xs lg:text-xs'>Loading...</p>
                 </div>
             )}
